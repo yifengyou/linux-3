2cdbf5c102c5e6d47939b3558bebcec0268df396
Matthew Garrett <matthew.garrett@nebula.com>
2012-03-09 09:28:15 -0500

    UBUNTU: SAUCE: UEFI: Restrict /dev/mem and /dev/kmem when module loading is restricted
    BugLink: http://bugs.launchpad.net/bugs/1566221
    BugLink: http://bugs.launchpad.net/bugs/1608854
    
    Allowing users to write to address space makes it possible for the kernel
    to be subverted, avoiding module loading restrictions. Prevent this when
    any restrictions have been imposed on loading modules.
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -159,6 +159,9 @@ static ssize_t write_mem(struct file *file, const char __user *buf,
 	unsigned long copied;
 	void *ptr;
 
+	if (secure_modules())
+		return -EPERM;
+
 	if (!valid_phys_addr_range(p, count))
 		return -EFAULT;
 
@@ -496,6 +499,9 @@ static ssize_t write_kmem(struct file *file, const char __user *buf,
 	ssize_t virtr = 0;
 	char *kbuf; /* k-addr because vwrite() takes vmlist_lock rwlock */
 	int err = 0;
+
+	if (secure_modules())
+		return -EPERM;
 
 	if (p < (unsigned long) high_memory) {
 		unsigned long to_write = min_t(unsigned long, count,
