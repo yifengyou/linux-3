1e6ec5402072570fc557acdd69002d681f7447fc
Matthew Garrett <matthew.garrett@nebula.com>
2013-08-09 17:58:15 -0400

    UBUNTU: SAUCE: UEFI: Add secure_modules() call
    BugLink: http://bugs.launchpad.net/bugs/1566221
    BugLink: http://bugs.launchpad.net/bugs/1608854
    
    Provide a single call to allow kernel code to determine whether the system
    has been configured to either disable module loading entirely or to load
    only modules signed with a trusted key.
    
    Bugzilla: N/A
    Upstream-status: Fedora mustard.  Replaced by securelevels, but that was nak'd
    
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>

--- include/linux/module.h
+++ include/linux/module.h
@@ -512,6 +512,8 @@ int unregister_module_notifier(struct notifier_block * nb);
 
 extern void print_modules(void);
 
+extern bool secure_modules(void);
+
 #else /* !CONFIG_MODULES... */
 
 /* Given an address, look for it in the exception tables. */
@@ -621,6 +623,11 @@ static inline int unregister_module_notifier(struct notifier_block * nb)
 
 static inline void print_modules(void)
 {
+}
+
+static inline bool secure_modules(void)
+{
+	return false;
 }
 #endif /* CONFIG_MODULES */
 
