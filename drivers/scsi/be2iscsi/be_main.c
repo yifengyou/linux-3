af0b4d0025a2d9231da17fcbd45c4d6f76921215
Jayamohan Kallickal <jayamohan.kallickal@emulex.com>
2014-01-29 02:16:44 -0500

    be2iscsi: Fix scsi_cmnd leakage in driver.
    BugLink: http://bugs.launchpad.net/bugs/1440156
    
    scsi_cmnd n io_task was not NULL when
     - Link goes down while IO was happening and session is closed.
     - Task for which TMF was sent.
    
    Signed-off-by: John Soni Jose <sony.john-n@emulex.com>
    Signed-off-by: Jayamohan Kallickal <jayamohan.kallickal@emulex.com>
    Reviewed-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <JBottomley@Parallels.com>
    (cherry picked from commit da3349771c7ca01415df49929b265c21bc4295c3)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- drivers/scsi/be2iscsi/be_main.c
+++ drivers/scsi/be2iscsi/be_main.c
@@ -1316,8 +1316,10 @@ be_complete_io(struct beiscsi_conn *beiscsi_conn,
 	resid = csol_cqe->res_cnt;
 
 	if (!task->sc) {
-		if (io_task->scsi_cmnd)
+		if (io_task->scsi_cmnd) {
 			scsi_dma_unmap(io_task->scsi_cmnd);
+			io_task->scsi_cmnd = NULL;
+		}
 
 		return;
 	}
@@ -1354,6 +1356,7 @@ be_complete_io(struct beiscsi_conn *beiscsi_conn,
 		conn->rxdata_octets += resid;
 unmap:
 	scsi_dma_unmap(io_task->scsi_cmnd);
+	io_task->scsi_cmnd = NULL;
 	iscsi_complete_scsi_task(task, exp_cmdsn, max_cmdsn);
 }
 
@@ -4602,6 +4605,11 @@ static void beiscsi_cleanup_task(struct iscsi_task *task)
 			free_io_sgl_handle(phba, io_task->psgl_handle);
 			spin_unlock(&phba->io_sgl_lock);
 			io_task->psgl_handle = NULL;
+		}
+
+		if (io_task->scsi_cmnd) {
+			scsi_dma_unmap(io_task->scsi_cmnd);
+			io_task->scsi_cmnd = NULL;
 		}
 	} else {
 		if (!beiscsi_conn->login_in_progress)
