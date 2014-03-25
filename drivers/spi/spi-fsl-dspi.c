2dab34f0de2c12c1e6bd7901853572e141f2752f
Axel Lin <axel.lin@ingics.com>
2014-03-25 09:19:05 +0800

    spi: fsl-dspi: Fix memory leak
    commit 0e0cd9ea8961b82947a40471080e7968b634820e upstream.
    
    The memory allocated for chip is not freed anywhere.
    Convert to use devm_kzalloc to fix the memory leak.
    
    Signed-off-by: Axel Lin <axel.lin@ingics.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/spi/spi-fsl-dspi.c
+++ drivers/spi/spi-fsl-dspi.c
@@ -336,7 +336,8 @@ static int dspi_setup_transfer(struct spi_device *spi, struct spi_transfer *t)
 	/* Only alloc on first setup */
 	chip = spi_get_ctldata(spi);
 	if (chip == NULL) {
-		chip = kcalloc(1, sizeof(struct chip_data), GFP_KERNEL);
+		chip = devm_kzalloc(&spi->dev, sizeof(struct chip_data),
+				    GFP_KERNEL);
 		if (!chip)
 			return -ENOMEM;
 	}
@@ -347,7 +348,6 @@ static int dspi_setup_transfer(struct spi_device *spi, struct spi_transfer *t)
 		fmsz = spi->bits_per_word - 1;
 	} else {
 		pr_err("Invalid wordsize\n");
-		kfree(chip);
 		return -ENODEV;
 	}
 
