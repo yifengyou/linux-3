49a4cb406f242a4ce97ff3d0aad05aa046bcc0d2
Chris Wilson <chris@chris-wilson.co.uk>
2015-08-31 15:10:39 +0100

    drm/i915: Always mark the object as dirty when used by the GPU
    BugLink: http://bugs.launchpad.net/bugs/1514832
    
    commit 51bc140431e233284660b1d22c47dec9ecdb521e upstream.
    
    There have been many hard to track down bugs whereby userspace forgot to
    flag a write buffer and then cause graphics corruption or a hung GPU
    when that buffer was later purged under memory pressure (as the buffer
    appeared clean, its pages would have been evicted rather than preserved
    and any changes more recent than in the backing storage would be lost).
    In retrospect this is a rare optimisation against memory pressure,
    already the slow path. If we always mark the buffer as dirty when
    accessed by the GPU, anything not used can still be evicted cheaply
    (ideal behaviour for mark-and-sweep eviction) but we do not run the risk
    of corruption. For correct read serialisation, userspace still has to
    notify when the GPU writes to an object. However, there are certain
    situations under which userspace may wish to tell white lies to the
    kernel...
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Kristian Høgsberg <krh@bitplanet.net>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
    Cc: "Goel, Akash" <akash.goel@intel.co>
    Cc: Michał Winiarski <michal.winiarski@intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    [ luis: backported to 3.16: adjusted context ]
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/gpu/drm/i915/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/i915_gem_execbuffer.c
@@ -912,6 +912,7 @@ i915_gem_execbuffer_move_to_active(struct list_head *vmas,
 		u32 old_read = obj->base.read_domains;
 		u32 old_write = obj->base.write_domain;
 
+		obj->dirty = 1; /* be paranoid  */
 		obj->base.write_domain = obj->base.pending_write_domain;
 		if (obj->base.write_domain == 0)
 			obj->base.pending_read_domains |= obj->base.read_domains;
@@ -920,7 +921,6 @@ i915_gem_execbuffer_move_to_active(struct list_head *vmas,
 
 		i915_vma_move_to_active(vma, ring);
 		if (obj->base.write_domain) {
-			obj->dirty = 1;
 			obj->last_write_seqno = intel_ring_get_seqno(ring);
 			if (obj->pin_count) /* check for potential scanout */
 				intel_mark_fb_busy(obj, ring);
