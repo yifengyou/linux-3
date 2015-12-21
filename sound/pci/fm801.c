a81b8ec6f94266e5b9cf45c466ce020648c65f6e
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2015-12-21 19:09:52 +0200

    ALSA: fm801: propagate TUNER_ONLY bit when autodetected
    BugLink: http://bugs.launchpad.net/bugs/1542497
    
    commit dbec6719ac036f68568d8488805d41346c021eff upstream.
    
    The commit d7ba858a7f7a (ALSA: fm801: implement TEA575x tuner autodetection)
    brings autodetection to the driver. However the autodetection algorithm misses
    the TUNER_ONLY bit if it is supplied by the user.
    
    Thus, user gets weird messages and no card registered.
    
     snd_fm801 0000:0d:01.0: detected TEA575x radio type SF64-PCR
     snd_fm801 0000:0d:01.0: AC'97 interface is busy (1)
     snd_fm801 0000:0d:01.0: AC'97 interface is busy (1)
    ...
     snd_fm801 0000:0d:01.0: AC'97 0 does not respond - RESET
     snd_fm801 0000:0d:01.0: AC'97 interface is busy (1)
     snd_fm801 0000:0d:01.0: AC'97 interface is busy (1)
     snd_fm801 0000:0d:01.0: AC'97 0 access is not valid [0x0], removing mixer.
     snd_fm801: probe of 0000:0d:01.0 failed with error -5
    
    Do a copy of TUNER_ONLY bit to be applied after autodetection is done.
    
    Fixes: d7ba858a7f7a (ALSA: fm801: implement TEA575x tuner autodetection)
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Cc: Ondrej Zary <linux@rainbow-software.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- sound/pci/fm801.c
+++ sound/pci/fm801.c
@@ -1272,6 +1272,8 @@ static int snd_fm801_create(struct snd_card *card,
 			return -ENODEV;
 		}
 	} else if ((tea575x_tuner & TUNER_TYPE_MASK) == 0) {
+		unsigned int tuner_only = tea575x_tuner & TUNER_ONLY;
+
 		/* autodetect tuner connection */
 		for (tea575x_tuner = 1; tea575x_tuner <= 3; tea575x_tuner++) {
 			chip->tea575x_tuner = tea575x_tuner;
@@ -1285,6 +1287,8 @@ static int snd_fm801_create(struct snd_card *card,
 			snd_printk(KERN_ERR "TEA575x radio not found\n");
 			chip->tea575x_tuner = TUNER_DISABLED;
 		}
+
+		chip->tea575x_tuner |= tuner_only;
 	}
 	if (!(chip->tea575x_tuner & TUNER_DISABLED)) {
 		strlcpy(chip->tea.card, get_tea575x_gpio(chip)->name,
