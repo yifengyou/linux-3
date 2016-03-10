1341731db0dd66047cbc8705b08e83d3936b08db
Andy Whitcroft <apw@canonical.com>
2016-03-10 10:26:37 +0000

    UBUNTU: [Packaging] reconstruct -- automatically reconstruct against base tag
    BugLink: http://bugs.launchpad.net/bugs/1764794
    OldBugLink: http://bugs.launchpad.net/bugs/1555543
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- debian/rules.d/1-maintainer.mk
+++ debian/rules.d/1-maintainer.mk
@@ -1,7 +1,7 @@
 # The following targets are for the maintainer only! do not run if you don't
 # know what they do.
 
-.PHONY: printenv updateconfigs printchanges insertchanges startnewrelease diffupstream help updateportsconfigs editportsconfigs finalchecks
+.PHONY: printenv updateconfigs printchanges insertchanges startnewrelease diffupstream help updateportsconfigs editportsconfigs autoreconstruct finalchecks
 
 help:
 	@echo "These are the targets in addition to the normal $(DEBIAN) ones:"
@@ -108,6 +108,9 @@ printchanges:
 
 insertchanges: finalchecks
 	@perl -w -f $(DROOT)/scripts/misc/insert-changes.pl $(DROOT) $(DEBIAN) 
+
+autoreconstruct:
+	$(DROOT)/scripts/misc/gen-auto-reconstruct $(release) >$(DEBIAN)/reconstruct
 
 finalchecks:
 	$(DROOT)/scripts/misc/final-checks "$(DEBIAN)" "$(prev_fullver)"
