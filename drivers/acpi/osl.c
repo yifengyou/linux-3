e2abada8ffa20d1407dc7cc807be5e498359ffd0
Tim Gardner <tim.gardner@canonical.com>
2016-08-23 07:23:27 -0600

    Revert "UBUNTU: SAUCE: UEFI: acpi: Ignore acpi_rsdp kernel parameter when module loading is restricted"
    BugLink: http://bugs.launchpad.net/bugs/1608854
    
    This reverts commit 49c48e5dffd632b7dc0f19d5417e3f74439f29e2.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/acpi/osl.c
+++ drivers/acpi/osl.c
@@ -45,7 +45,6 @@
 #include <linux/list.h>
 #include <linux/jiffies.h>
 #include <linux/semaphore.h>
-#include <linux/module.h>
 
 #include <asm/io.h>
 #include <asm/uaccess.h>
@@ -250,7 +249,7 @@ early_param("acpi_rsdp", setup_acpi_rsdp);
 acpi_physical_address __init acpi_os_get_root_pointer(void)
 {
 #ifdef CONFIG_KEXEC
-	if (acpi_rsdp && !secure_modules())
+	if (acpi_rsdp)
 		return acpi_rsdp;
 #endif
 
