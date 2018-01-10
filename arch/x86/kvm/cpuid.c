9a97dae0be55385ca055cee1c2d3eef0e10fd16b
Tom Lendacky <thomas.lendacky@amd.com>
2018-01-10 18:51:48 +0100

    UBUNTU: SAUCE: KVM: x86: Add speculative control CPUID support for guests
    CVE-2017-5715 (Spectre v2 Intel)
    
    Provide the guest with the speculative control CPUID related values.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    (backported (merge some required context to support additional cpuid
    leaves))
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -23,6 +23,10 @@
 #include "mmu.h"
 #include "trace.h"
 
+/* These are scattered features in cpufeatures.h. */
+#define KVM_CPUID_BIT_SPEC_CTRL		26
+#define KF(x) bit(KVM_CPUID_BIT_##x)
+
 static u32 xstate_required_size(u64 xstate_bv)
 {
 	int feature_bit = 0;
@@ -312,6 +316,14 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 		F(FSGSBASE) | F(BMI1) | F(HLE) | F(AVX2) | F(SMEP) |
 		F(BMI2) | F(ERMS) | f_invpcid | F(RTM);
 
+	/* cpuid 7.0.edx*/
+	const u32 kvm_cpuid_7_0_edx_x86_features =
+		KF(SPEC_CTRL);
+
+	/* cpuid 0x80000008.0.ebx */
+	const u32 kvm_cpuid_80000008_0_ebx_x86_features =
+		F(IBPB);
+
 	/* all calls to cpuid_count() should be made on the same cpu */
 	get_cpu();
 
@@ -383,11 +395,14 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 			cpuid_mask(&entry->ebx, 9);
 			// TSC_ADJUST is emulated
 			entry->ebx |= F(TSC_ADJUST);
-		} else
+			entry->edx &= kvm_cpuid_7_0_edx_x86_features;
+			entry->edx &= get_scattered_cpuid_leaf(7, 0, 3);
+		} else {
 			entry->ebx = 0;
+			entry->edx = 0;
+		}
 		entry->eax = 0;
 		entry->ecx = 0;
-		entry->edx = 0;
 		break;
 	}
 	case 9:
@@ -503,7 +518,9 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 		if (!g_phys_as)
 			g_phys_as = phys_as;
 		entry->eax = g_phys_as | (virt_as << 8);
-		entry->ebx = entry->edx = 0;
+		entry->ebx &= kvm_cpuid_80000008_0_ebx_x86_features;
+		cpuid_mask(&entry->ebx, 13);
+		entry->edx = 0;
 		break;
 	}
 	case 0x80000019:
