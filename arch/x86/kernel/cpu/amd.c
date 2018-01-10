b4cafde854f9047ec06208c032d1f5480f519b13
Tom Lendacky <thomas.lendacky@amd.com>
2018-01-10 17:31:59 +0100

    UBUNTU: SAUCE: x86/cpu/AMD: Add speculative control support for AMD
    CVE-2017-5715 (Spectre v2 Intel)
    
    Add speculative control support for AMD processors. For AMD, speculative
    control is indicated as follows:
    
      CPUID EAX=0x00000007, ECX=0x00 return EDX[26] indicates support for
      both IBRS and IBPB.
    
      CPUID EAX=0x80000008, ECX=0x00 return EBX[12] indicates support for
      just IBPB.
    
    On AMD family 0x10, 0x12 and 0x16 processors where either of the above
    features are not supported, IBPB can be achieved by disabling
    indirect branch predictor support in MSR 0xc0011021[14] at boot.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    (backported (context))
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -762,6 +762,45 @@ static void init_amd(struct cpuinfo_x86 *c)
 	if (cpu_has_amd_erratum(c, amd_erratum_400))
 		set_cpu_bug(c, X86_BUG_AMD_APIC_C1E);
 
+	/* AMD speculative control support */
+	if (cpu_has(c, X86_FEATURE_SPEC_CTRL)) {
+		pr_info_once("FEATURE SPEC_CTRL Present\n");
+		set_ibrs_supported();
+		set_ibpb_supported();
+		if (ibrs_inuse)
+			sysctl_ibrs_enabled = 1;
+		if (ibpb_inuse)
+			sysctl_ibpb_enabled = 1;
+	} else if (cpu_has(c, X86_FEATURE_IBPB)) {
+		pr_info_once("FEATURE SPEC_CTRL Not Present\n");
+		pr_info_once("FEATURE IBPB Present\n");
+		set_ibpb_supported();
+		if (ibpb_inuse)
+			sysctl_ibpb_enabled = 1;
+	} else {
+		pr_info_once("FEATURE SPEC_CTRL Not Present\n");
+		pr_info_once("FEATURE IBPB Not Present\n");
+		/*
+		 * On AMD processors that do not support the speculative
+		 * control features, IBPB type support can be achieved by
+		 * disabling indirect branch predictor support.
+		 */
+		if (!ibpb_disabled) {
+			u64 val;
+
+			switch (c->x86) {
+			case 0x10:
+			case 0x12:
+			case 0x16:
+				pr_info_once("Disabling indirect branch predictor support\n");
+				rdmsrl(MSR_F15H_IC_CFG, val);
+				val |= MSR_F15H_IC_CFG_DIS_IND;
+				wrmsrl(MSR_F15H_IC_CFG, val);
+				break;
+			}
+		}
+	}
+
 	rdmsr_safe(MSR_AMD64_PATCH_LEVEL, &c->microcode, &dummy);
 }
 
