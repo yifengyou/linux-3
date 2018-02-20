3d593f8d2e8c3310e187bcbc7f6941c7955b0824
Andy Whitcroft <apw@canonical.com>
2018-02-20 13:53:53 +0000

    Revert "UBUNTU: SAUCE: userns: prevent speculative execution"
    CVE-2017-5753 (revert embargoed)
    CVE-2017-5715 (revert embargoed)
    
    This reverts commit a0dd5b93345fbfc0d51ff1ce98fbc17e53c2597b.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- kernel/user_namespace.c
+++ kernel/user_namespace.c
@@ -499,10 +499,8 @@ static void *m_start(struct seq_file *seq, loff_t *ppos, struct uid_gid_map *map
 	struct uid_gid_extent *extent = NULL;
 	loff_t pos = *ppos;
 
-	if (pos < map->nr_extents) {
-		gmb();
+	if (pos < map->nr_extents)
 		extent = &map->extent[pos];
-	}
 
 	return extent;
 }
