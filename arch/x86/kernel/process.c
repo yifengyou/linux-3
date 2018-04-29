8179bc0ca426bac27e5678a32074fc8173787706
Thomas Gleixner <tglx@linutronix.de>
2018-04-29 15:21:42 +0200

    x86/process: Allow runtime control of Speculative Store Bypass
    The Speculative Store Bypass vulnerability can be mitigated with the
    Reduced Data Speculation (RDS) feature. To allow finer grained control of
    this eventually expensive mitigation a per task mitigation control is
    required.
    
    Add a new TIF_RDS flag and put it into the group of TIF flags which are
    evaluated for mismatch in switch_to(). If these bits differ in the previous
    and the next task, then the slow path function __switch_to_xtra() is
    invoked. Implement the TIF_RDS dependent mitigation control in the slow
    path.
    
    If the prctl for controlling Speculative Store Bypass is disabled or no
    task uses the prctl then there is no overhead in the switch_to() fast
    path.
    
    Update the KVM related speculation control functions to take TID_RDS into
    account as well.
    
    Based on a patch from Tim Chen. Completely rewritten.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
    CVE-2018-3639 (x86)
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    [smb: msr-index->arch/x86/include/uapi/asm/
          arch/x86/kernel/process.c no flags pre-fetched and
          _TIF_WORK_MASK and _TIF_ALLWORK_MASK need to exclude new flag]
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -19,6 +19,7 @@
 #include <linux/cpuidle.h>
 #include <trace/events/power.h>
 #include <linux/hw_breakpoint.h>
+#include <asm/spec-ctrl.h>
 #include <asm/cpu.h>
 #include <asm/apic.h>
 #include <asm/syscalls.h>
@@ -196,6 +197,24 @@ int set_tsc_mode(unsigned int val)
 	return 0;
 }
 
+static __always_inline void __speculative_store_bypass_update(unsigned long tifn)
+{
+	u64 msr;
+
+	if (static_cpu_has(X86_FEATURE_AMD_RDS)) {
+		msr = x86_amd_ls_cfg_base | rds_tif_to_amd_ls_cfg(tifn);
+		wrmsrl(MSR_AMD64_LS_CFG, msr);
+	} else {
+		msr = x86_spec_ctrl_base | rds_tif_to_spec_ctrl(tifn);
+		wrmsrl(MSR_IA32_SPEC_CTRL, msr);
+	}
+}
+
+void speculative_store_bypass_update(void)
+{
+	__speculative_store_bypass_update(current_thread_info()->flags);
+}
+
 void __switch_to_xtra(struct task_struct *prev_p, struct task_struct *next_p,
 		      struct tss_struct *tss)
 {
@@ -223,6 +242,10 @@ void __switch_to_xtra(struct task_struct *prev_p, struct task_struct *next_p,
 		else
 			hard_enable_TSC();
 	}
+
+	if (test_tsk_thread_flag(prev_p, TIF_RDS) ^
+	    test_tsk_thread_flag(next_p, TIF_RDS))
+		__speculative_store_bypass_update(task_thread_info(next_p)->flags);
 
 	if (test_tsk_thread_flag(next_p, TIF_IO_BITMAP)) {
 		/*
