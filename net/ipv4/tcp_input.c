16b8d33ce1b2c0f83c384eda1a9aa70c802d9ee5
Tyler Hicks <tyhicks@canonical.com>
2018-09-14 22:47:00 +0200

    UBUNTU: SAUCE: tcp: Correct the backport of the CVE-2018-5390 fix
    The backport of upstream commit 3d4bf93ac120 ("tcp: detect malicious
    patterns in tcp_collapse_ofo_queue()") didn't correctly reset
    range_truesize when moving on to a new range and didn't increase
    range_truesize when operating within a range.
    
    CVE-2018-5390
    
    Fixes: 8a668da92a76 ("tcp: detect malicious patterns in tcp_collapse_ofo_queue()")
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Marcelo Cerri <marcelo.cerri@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4636,8 +4636,9 @@ static void tcp_collapse_ofo_queue(struct sock *sk)
 			/* Start new segment */
 			start = TCP_SKB_CB(skb)->seq;
 			end = TCP_SKB_CB(skb)->end_seq;
-			range_truesize += skb->truesize;
+			range_truesize = skb->truesize;
 		} else {
+			range_truesize += skb->truesize;
 			if (before(TCP_SKB_CB(skb)->seq, start))
 				start = TCP_SKB_CB(skb)->seq;
 			if (after(TCP_SKB_CB(skb)->end_seq, end))
